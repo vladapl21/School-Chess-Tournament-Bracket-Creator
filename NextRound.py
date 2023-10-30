43a8244bdafb25cff175905c04856ccc2a88aed9
Vlad Plyushchenko <vladplyushchenko@gmail.com>
2023-10-30 14:27:24 +0300

    Added sorting matchups based on round number to next_round function

--- NextRound.py
+++ NextRound.py
@@ -1,5 +1,8 @@
-def next_round(sorted_leaderboard):
-    table = [["White", "Black"]]
+def next_round(sorted_leaderboard, round_num):
+    if round_num % 2 == 0:
+        table = [["Black", "White"]]
+    else:
+        table = [["White", "Black"]]
     for i in range(0, len(sorted_leaderboard) - 1, 2):
         table.append([sorted_leaderboard[i][0], sorted_leaderboard[i+1][0]])
     return table
