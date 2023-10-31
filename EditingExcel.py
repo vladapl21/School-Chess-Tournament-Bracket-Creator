772f4b8d0d8b21237434410c09ba8d82916c50e3
Vlad Plyushchenko <vladplyushchenko@gmail.com>
2023-10-31 17:36:37 +0300

    Sorted next round matches to an excel file
    Used OpenPyXL to create new sheet in excel and iterate the next round matchups to it for round 7

--- EditingExcel.py
+++ EditingExcel.py
@@ -1,5 +1,17 @@
 from openpyxl import Workbook, load_workbook
+from main import next_round_matches
 
 wb = load_workbook("Chess_Tournament.xlsx")
-ws = wb.active
-print(ws["A2"].value)
+ws = wb["Round 7"]
+
+
+def import_round(next_round):
+    for row in range(1, 53):
+        for col in range(2):
+            char = chr(65 + col)
+            ws[str(char) + str(row)] = next_round[row-1][col]
+
+
+import_round(next_round_matches)
+
+wb.save("Chess_Tournament.xlsx")
